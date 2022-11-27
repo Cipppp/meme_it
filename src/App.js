b236d0385f1448dcf892ed6a719834916cab7b73
Cipppp <pirvuciprian37@gmail.com>
2022-11-27 18:08:14 +0200

    Added main files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './components/Homepage';
+
+function App() {
+    return (
+        <>
+            <Homepage />
+        </>
+    );
+}
+
+export default App;
